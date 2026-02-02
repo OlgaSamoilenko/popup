4b9639764a92fada1ec9b0c75f636a3e11e208d5
OlgaSamoilenko <70361687+OlgaSamoilenko@users.noreply.github.com>
2026-02-02 17:02:31 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -32,3 +32,30 @@ buyBtn.addEventListener("click", () => {
   success.style.display = "none";
   overlay.classList.remove("show");
 });
+
+
+
+var links = document.querySelectorAll('.link li');
+var content = document.querySelectorAll('.tabs-content .tabs_box');
+for(var i=0; i <links.length; i++) {
+    (function(i) {
+       var link = links[i];
+       link.onclick = function() {
+           for(var j=0; j <content.length; j++) {
+              var opacity = window.getComputedStyle(content[j]).opacity;
+              if(opacity == "1") {
+                 content[j].style.opacity = "0";
+              }
+           }
+        content[i].style.opacity = "1";
+        }
+    })(i);
+}
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// const tasks = ['встать', 'купить', 'продать'];
+// for(let i=0; i < 3; i++){
+//   console.log(tasks[i]);
+// }
